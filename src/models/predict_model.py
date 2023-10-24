7ef34f2783ece0bd43ea7f14c24371d0bd38d8e6
simo_lenovo <gramegna.simone@gmail.com>
2023-10-24 18:31:34 +0200

    completato report

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,44 +1,52 @@
 import joblib
 from train_model import read_data
 from pathlib import Path
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import json
+import mlflow
+import mlflow.sklearn
 
 PROJECT_PATH = str(Path(Path(__file__).resolve().parents[2]))
 MODEL_PATH = PROJECT_PATH + "\models"
+REPORT_PATH = PROJECT_PATH + "\\reports"
 
-def predict():
+report_json_file = REPORT_PATH + "\\classification_report.json"
+report_confusion_matrix = REPORT_PATH + "\\confusion_matrix.json"
 
-    model= joblib.load(MODEL_PATH+"\\model.pkl")
+def predict_evaluate():
+
+    mlflow.start_run()
+
+    model = joblib.load(MODEL_PATH+"\\model.pkl")
     X_train, X_test, y_train, y_test = read_data()
+
     y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred, target_names=['safe_URL', 'unsafe_URL']))
+    report = classification_report(y_test, y_pred, target_names=['safe_URL', 'unsafe_URL'], output_dict=True)
+    confusion_matrix_report = confusion_matrix(y_test, y_pred)
+
+  
+    mlflow.log_artifact(MODEL_PATH, "model.pkl")
+
+    # Save the classification report to a JSON file
+    classification_report_json = json.dumps(report, indent=4)
+    with open(report_json_file, "w") as report_file:
+        report_file.write(classification_report_json)
+
+       
+    # Save the confusion matrix report to a JSON file
+    confusion_matrix_dict = {"confusion_matrix": confusion_matrix_report.tolist()}
+    confusion_matrix_json = json.dumps(confusion_matrix_dict, indent=4)
+    with open(report_confusion_matrix, "w") as confusion_matrix_file:
+        confusion_matrix_file.write(confusion_matrix_json)
+        
+        
+
+
+
     
-    report = classification_report(y_test, y_pred, target_names=['safe_URL', 'unsafe_URL'])
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = report['weighted avg']['precision']
-    recall = report['weighted avg']['recall']
-    f1 = report['weighted avg']['f1-score']
-
-    # Create a dictionary to store metrics
-    metrics = {
-        "accuracy": accuracy,
-        "precision": precision,
-        "recall": recall,
-        "f1_score": f1
-    }
-
-    # Define the filename to save the metrics as JSON
-    metrics_filename = "random_forest_metrics.json"
-
-    # Write the metrics to a JSON file
-    with open(metrics_filename, "w") as json_file:
-        json.dump(metrics, json_file, indent=4)
-
-    print(f"Metrics saved to '{metrics_filename}")
 
 
 
 
 if __name__ == '__main__':
-    predict()
+    predict_evaluate()
