e7aa9be5b4b12826cc3f9b95bd7368deb1283977
simo_lenovo <gramegna.simone@gmail.com>
2023-12-04 17:02:05 +0100

    updated docker compose file

--- SvelteApp/vite.config.ts
+++ SvelteApp/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [sveltekit()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000", // Cambia l'URL se il server Flask è in esecuzione su una porta diversa
+      "/api": "http://flask_api:5000", // Cambia l'URL se il server Flask è in esecuzione su una porta diversa
     },
   },
 });
