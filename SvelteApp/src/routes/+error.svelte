ea8188d10b9d8824882823eafebf851499040131
Ferdo <ferdosama@outlook.it>
2023-12-12 16:40:19 +0100

    error page added

---
+++ SvelteApp/src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Button from "$lib/components/Button.svelte";
+</script>
+
+<div class="error">
+  <h2>Qualcosa è andato storto!</h2>
+  <a href="/"><Button>Torna indietro</Button></a>
+</div>
+
+<style>
+  .error {
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+  }
+</style>
